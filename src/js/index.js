e17fd86e8506d0fafe911871b0f8c44b3ee100b7
doinel1a <doinel1atanasiu@gmail.com>
2023-03-25 14:01:42 +0100

    refactor: index, css, js

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,14 @@
 import '../css/global.css';
 import '../scss/global.scss';
 
-import config from '../../_config.cjs';
 import Three from './three';
 
-const githubCornerTitle = 'Get started on GitHub';
-const githubRepoUrl =
-  'https://github.com/doinel1a/vite-vanilla-three-js-starter';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const title = document.querySelector('#title');
-  const main = document.querySelector('#main');
-  const githubCorner = main.querySelector('#github');
-
-  title.textContent = config.title;
-
-  githubCorner.setAttribute('title', githubCornerTitle);
-  githubCorner.setAttribute('aria-label', githubCornerTitle);
-  githubCorner.setAttribute('href', githubRepoUrl);
-});
+document.addEventListener('DOMContentLoaded', () => {});
 
 window.addEventListener('load', () => {
-  new Three(document.querySelector('#canvas'));
+  const canvas = document.querySelector('#canvas');
+
+  if (canvas) {
+    new Three(document.querySelector('#canvas'));
+  }
 });
