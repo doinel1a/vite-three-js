6fb6789afcb14238e4424f7c966a5789950fbf59
doinel1a <doinel1atanasiu@gmail.com>
2023-03-26 00:30:20 +0100

    chore: dev server host & port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import { partytownVite } from '@builder.io/partytown/utils';
 import legacy from '@vitejs/plugin-legacy';
 import glsl from 'vite-plugin-glsl';
 
+import _config from './_config';
+
 export default {
+  server: {
+    host: _config.server.host,
+    port: _config.server.port
+  },
   plugins: [
     legacy(),
     glsl(),
